90314cef69f2d8dc08197a12141e39db818f5669
sleepylee02 <jaeyoung02@yonsei.ac.kr>
2025-08-20 01:26:43 +0900

    integration2

--- obsidian-plugin/src/autocomplete/codemirrorIntegration-stub.ts
+++
@@ -1,8 +0,0 @@
-// Stub for autocomplete integration
-export const DEFAULT_AUTOCOMPLETE_SETTINGS = {
-  isEnabled: false,
-};
-
-export function initializeAutoComplete() {
-  // Stub implementation
-}
